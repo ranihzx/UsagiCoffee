f97b342211fcac6c4615e38713dc017225477a7b
ranihzx <ranieleconceicaope@gmail.com>
2026-04-05 16:08:47 -0300

    supabase

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 
-const supabaseUrl = "https://txygeusmlfgftsqpcfnj.supabase.co";
-const supabaseAnonKey = "sb_publishable_dBUUX27Ti5jfea1-xcNBCQ_S6vvrnhq";
+const supabaseUrl = "https://gtshtxainaadahuyiwzd.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd0c2h0eGFpbmFhZGFodXlpd3pkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU0MDU1NjIsImV4cCI6MjA5MDk4MTU2Mn0.DenkSOeCz905kk5ZmZJJPWqhTJDyRIlOuCd-WYrmqPs";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
